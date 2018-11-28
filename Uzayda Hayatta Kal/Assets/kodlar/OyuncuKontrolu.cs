03642207ed1f72ad6e5bc25b72289b9751c12085
Mehmet Arikan <me@mehmetarika.net>
2018-11-28 23:21:46 +0300

    first commit with love

---
+++ Uzayda Hayatta Kal/Assets/kodlar/OyuncuKontrolu.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OyuncuKontrolu : MonoBehaviour {
+
+    Rigidbody fizik;
+	float horizontal = 0;
+	float vertical = 0;
+	Vector3 vec;
+	void Start () {
+
+		fizik = GetComponent<Rigidbody> ();
+
+       
+		
+	}
+	
+	
+	void FixedUpdate () {
+		horizontal=Input.GetAxis("Horizontal");
+		vertical=Input.GetAxis("Vertical");
+		vec = new Vector3(horizontal,0,vertical);
+
+		fizik.velocity = vec;
+	}
+}
