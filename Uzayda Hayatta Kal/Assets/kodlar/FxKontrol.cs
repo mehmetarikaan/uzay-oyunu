e526cc9622d0a6070a271ad1e09e75940d1b36dc
Mehmet Arikan <me@mehmetarika.net>
2018-12-06 00:52:18 +0300

    sinir
    Sinir ve kursun

---
+++ Uzayda Hayatta Kal/Assets/kodlar/FxKontrol.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FxKontrol : MonoBehaviour {
+
+	Rigidbody fizik;
+	public float hiz;
+	void Start () {
+		
+		fizik = GetComponent<Rigidbody> ();
+		fizik.velocity = transform.forward*hiz;
+	}
+	
+
+	
+}
--- Uzayda Hayatta Kal/Assets/kodlar/OyuncuKontrolu.cs
+++ Uzayda Hayatta Kal/Assets/kodlar/OyuncuKontrolu.cs
@@ -3,11 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class OyuncuKontrolu : MonoBehaviour {
-
+	
     Rigidbody fizik;
 	float horizontal = 0;
 	float vertical = 0;
 	Vector3 vec;
+	public float karakterHiz;
+	public float minX;
+	public float maxX;
+	public float minZ;
+	public float maxZ;
+	public float egim;
+	float atesZamani = 0;
+	public float atesGecenZamani;
+	public GameObject Kursun;
+	public Transform KursunNeredenCiksin;
+
 	void Start () {
 
 		fizik = GetComponent<Rigidbody> ();
@@ -16,12 +27,32 @@ public class OyuncuKontrolu : MonoBehaviour {
 		
 	}
 	
-	
+	void Update()
+	{
+		if(Input.GetButton("Fire1")&& Time.time>atesZamani)
+		{
+			atesZamani = Time.time+ atesGecenZamani;
+			Debug.Log("AtesEdildi");
+			Instantiate(Kursun, KursunNeredenCiksin.position,Quaternion.identity);
+		}
+	}
+
+
+
 	void FixedUpdate () {
 		horizontal=Input.GetAxis("Horizontal");
 		vertical=Input.GetAxis("Vertical");
 		vec = new Vector3(horizontal,0,vertical);
 
-		fizik.velocity = vec;
+		fizik.velocity = vec*karakterHiz;
+
+		fizik.position = new Vector3 
+		(
+			Mathf.Clamp(fizik.position.x,minX,maxX),
+			0.0f,
+			Mathf.Clamp(fizik.position.z,minZ,maxZ)
+		);
+
+		fizik.rotation = Quaternion.Euler(0,0,fizik.velocity.x*-egim);
 	}
 }
